3ffcab91d0befaa67903af2b99ccf91b300f1e1a
TikTokGG67439 <s3ventt@gmail.com>
2025-09-11 17:09:44 +0300

    StealYoutubers.lua

--- StealYoutubers.lua
+++ StealYoutubers.lua
@@ -285,7 +285,7 @@ local function setupCharacter(char)
 					end
 
 					-- Ждём 0.2 секунды и отключаем временно включенные Anchor и Noclip
-					task.wait(0.2)
+					task.wait(1)
 					if not anchorWasOn then
 						buttonStates.Anchor.Value = false
 						anchorOnly = false
